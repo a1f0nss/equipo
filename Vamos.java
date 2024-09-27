06ba9f674c301f1784782dc6fb40000af97bcef3
alfonss90@gmail.com <alfonss90@gmail.com>
2024-09-27 18:33:09 +0200

    ampliacion

--- Vamos.java
+++ Vamos.java
@@ -3,6 +3,7 @@ public class Vamos {
   public static void main(String[] args) {
     System.out.println("Vamos palante");
 	System.out.println("Vamos patras");
+System.out.printls('vamos pa el centro');
   }
 
 }
